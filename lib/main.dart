fb21bc7ab36cc4b4805792ef76c9b255a87c3e3d
JSiziba <siziba.zw@gmail.com>
2025-07-25 19:52:36 +0200

    Force vertical orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:smsgateway/custom_colors.dart';
 import 'package:smsgateway/service_locator.dart';
-import 'package:smsgateway/services/sms_service.dart';
 import 'package:smsgateway/sms_gateway.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   await setupServiceLocator();
   runApp(const MyApp());
 }
